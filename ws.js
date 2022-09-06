d6434566e91b9af0d3961f4b7aea5d750587d606
bferster <bferster@stagetools.com>
2022-09-06 13:10:21 -0400

    express server

--- ws.js
+++ ws.js
@@ -6,7 +6,7 @@
 	npm install express	npm install forever
 	open port: 3000
 	localhost: node sql.js
-	server: cd /opt/bitnami/wordpress/sql/ws.js | forever stop ws.js | forever start ws.js 
+	server: cd /opt/bitnami/wordpress/sql | forever stop ws.js | forever start ws.js 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////  */
 
@@ -20,7 +20,7 @@
 		console.log("Express server running on port 3000");
 		});
 
-		app.get("/paGetByEmail", (req, res, next) => {								// ON GET BY EMAIL
+		app.get("/sql/paGetByEmail", (req, res, next) => {								// ON GET BY EMAIL
 			if (req && req.query && req.query.email) {									// If valid params
 				Open();																	// Open SQL
 				db.serialize(() => {													// Serialize
@@ -33,7 +33,7 @@
 				}
 			 });
 	
-		app.get("/paGetById", (req, res, next) => {									// ON GET BY ID
+		app.get("/sql/paGetById", (req, res, next) => {									// ON GET BY ID
 			if (req && req.query && req.query.id) {										// If valid params
 				Open();																	// Open SQL
 				db.all(`SELECT * FROM pa WHERE id = '${req.query.id}'`, (err, row) =>{ 	// Query DB
